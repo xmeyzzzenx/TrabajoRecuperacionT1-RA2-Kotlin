68ceb134e388a3459f4ab69c2c4f9dd6722df22c
Ximena Meyzen <xmeyzzzenx@gmail.com>
2026-04-14 20:19:15 +0200

    Comparación entre ejecución secuencial y concurrente con hilos

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,12 +1,40 @@
 fun main() {
-    println("Inicio del programa")
+    println("Secuencial:")
 
-    val hilo = Thread {
-        println("Hola desde el hilo")
+    val inicioSecuencial = System.currentTimeMillis()
+
+    tarea("A")
+    tarea("B")
+
+    val finSecuencial = System.currentTimeMillis()
+
+    println("Tiempo secuencial: ${finSecuencial - inicioSecuencial} ms")
+
+    println("\nConcurrente:")
+
+    val inicioConcurrente = System.currentTimeMillis()
+
+    val hilo1 = Thread {
+        tarea("A")
     }
 
-    hilo.start()
-    hilo.join()
+    val hilo2 = Thread {
+        tarea("B")
+    }
 
-    println("Fin del programa")
+    hilo1.start()
+    hilo2.start()
+
+    hilo1.join()
+    hilo2.join()
+
+    val finConcurrente = System.currentTimeMillis()
+
+    println("Tiempo concurrente: ${finConcurrente - inicioConcurrente} ms")
+}
+
+fun tarea(nombre: String) {
+    println("Tarea $nombre iniciada")
+    Thread.sleep(2000)
+    println("Tarea $nombre terminada")
 }
